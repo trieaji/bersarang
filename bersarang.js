bb83191f24b9d27abb19c00aa88248e4a5db42dc
trieaji <laksabayu18@gmail.com>
2021-11-04 14:45:20 +0700

    first commit

---
+++ bersarang.js
@@ -0,0 +1,57 @@
+/*var t = 0;
+while( t < 5) {
+	console.log('*');
+t++;	
+}*/
+
+/*
+var s = '';
+for( var i = 1; i <= 5; i++ ) {
+	for( var j = 0; j < 4 ; j++) {
+		s += 'Laksa' + i;
+	}
+	s += '\n';
+
+	
+}
+console.log(s);
+*/
+
+/*
+var k = '';
+for (var l = 6; l > 0; l--) {
+	for (var z = 0; z < l; z++) {
+		k += '*' + l;	
+	}
+
+	
+	k += '\n';
+
+}console.log(k);
+*/
+
+/*
+var masuk = prompt('Apa yang ingin kamu ucapkan?');
+var s = '';
+var mandek = 1;
+while(mandek <= 3) {
+	s += masuk;
+
+	s += '\n';
+mandek++;
+}console.log(s);
+*/
+
+
+var masuk = prompt('Ucapkan saja');
+var s = '';
+
+for(var z = 1; z <= 5; z++) {
+
+	
+
+	s += masuk;
+	s += '\n';
+} console.log(s);
+
+
